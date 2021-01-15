4aac57a58cc264fd63789e4e32a2122e5299d0ba
AymericBethencourt <aymeric.bethencourt@gmail.com>
2021-01-15 16:13:30 +0100

    Correction part 2 complete

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -15,31 +15,40 @@ contract('TokenFarm', ([owner, investor]) => {
 
   before(async () => {
     // Load Contracts
+    daiToken = await DaiToken.new()
+    dappToken = await DappToken.new()
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
 
     // Transfer all Dapp tokens to farm (1 million)
+    await dappToken.transfer(tokenFarm.address, tokens('1000000'))
 
     // Send tokens to investor
+    await daiToken.transfer(investor, tokens('100'), { from: owner })
   })
 
   describe('Mock DAI deployment', async () => {
     it('has a name', async () => {
-    // Todo
+      const name = await daiToken.name()
+      assert.equal(name, 'Mock DAI Token')
     })
   })
 
   describe('Dapp Token deployment', async () => {
     it('has a name', async () => {
-      // Todo
+      const name = await dappToken.name()
+      assert.equal(name, 'DApp Token')
     })
   })
 
   describe('Token Farm deployment', async () => {
     it('has a name', async () => {
-      // Todo
+      const name = await tokenFarm.name()
+      assert.equal(name, 'Dapp Token Farm')
     })
 
     it('contract has tokens', async () => {
-      // Todo
+      let balance = await dappToken.balanceOf(tokenFarm.address)
+      assert.equal(balance.toString(), tokens('1000000'))
     })
   })
 
